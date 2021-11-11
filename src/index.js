c81426e5093811c0be5e8b90a15fc16b9ad6fec0
Paul Shanahan <pshaners10@gmail.com>
2021-11-11 11:01:58 +0000

    Fix lab 4 bugs.

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import HomePage from "./pages/homePage";
+import MoviePage from "./pages/movieDetailsPage";
+import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
+import MovieReviewPage from "./pages/movieReviewPage";
+import Upcoming from "./pages/upcoming";
+import SiteHeader from './components/siteHeader';
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
+import MoviesContextProvider from "./contexts/moviesContext";
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000, 
+      refetchOnWindowFocus: false
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+     <SiteHeader />   <MoviesContextProvider>   
+          
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/movies/favorites">Favorites</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/movies/upcoming" component={Upcoming} />
+        <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+        <Route path="/movies/:id" component={MoviePage} />
+        <Route path="/reviews/:id" component={MovieReviewPage} />
+        <Route exact path="/" component={HomePage} />
+        <Redirect from="*" to="/" />
+      </Switch>
+      </MoviesContextProvider>
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
